1a88332f395ff12264b571602353bb2071d4fba6
Trevor M <trevordmooney@gmail.com>
2025-07-02 17:47:36 -0700

    fix: continuing to build and test multiplayer functionality

--- src/main/java/org/mastermind/Multiplayer.java
+++ src/main/java/org/mastermind/Multiplayer.java
@@ -1,9 +1,6 @@
 package org.mastermind;
 
-import java.io.BufferedReader;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.io.OutputStream;
+import java.io.*;
 import java.util.Scanner;
 import java.net.ServerSocket;
 import java.net.Socket;
@@ -70,24 +67,154 @@ public class Multiplayer {
     class Server implements Runnable {
         ServerSocket listener;
 
-        public void run(){
+        public void run() {
             Socket socket = null;
             try {
                 listener = new ServerSocket(gamePort);
-                try{
-                    listener = new ServerSocket(gamePort);
-                    while(keepListening)
-                    {
-                        socket = listener.accept();
-                        InputStream input = socket.getInputStream();
-                        BufferedReader reader = new BufferedReader(new InputStreamReader(input));
-                        OutputStream output = socket.getOutputStream();
+                while (keepListening) {
+                    socket = listener.accept();
+                    InputStream input = socket.getInputStream();
+                    BufferedReader reader = new BufferedReader(new InputStreamReader(input));
+                    OutputStream output = socket.getOutputStream();
+                    PrintWriter writer = new PrintWriter(output, true); //autoflush
+
+                    while (startNewGame) {
+                        //*** creates a new game, but assumes this will be the last
+                        writer.println("NEW_GAME");
+                        startNewGame = false;
+
+                        //*** if client agrees, send over the location of the trees
+                        String response = reader.readLine();
+                        if (response != null && response.equals("OK")) {
+                            gameField.setupNewGame();
+                            //loop through writing tree positions
+                        } else {
+                            System.err.println("Unxpected start response: " + response);
+                            System.err.println("Skipping game and waiting again.");
+                            keepPlaying = false;
+                            break;
+                        }
+
+                        //Start the action!
+                        writer.println("START");
+                        response = reader.readLine();
+                        keepPlaying = response.equals("OK");
+
+                        while (keepPlaying) {
+                            try {
+                                if (gameField.trees.size() > 0) {
+                                    writer.print("SCORE");
+                                } else {
+                                    write.print("END ");
+                                    keepPlaying = false;
+                                }
+                                writer.println(gameField.getScore(1));
+                                response = reader.readLine();
+                                if (response == null) {
+                                    keepPlaying = false;
+                                    disconnecting = true;
+                                } else {
+                                    String parts[] = response.split(" ");
+                                    switch (parts[0]) {
+                                        case "END":
+                                            keepPlaying = false;
+                                        case "SCORE":
+                                            gameField.setScore(2, parts[1]);
+                                            break;
+                                        case "DISCONNECT":
+                                            disconnecting = true;
+                                            keepPlaying = false;
+                                            break;
+                                        default:
+                                            System.err.println("Warning. Unexpected command: "
+                                                    + parts[0] + ". Ignoring.");
+                                    }
+                                }
+                                Thread.sleep(500);
+                            } catch (InterruptedException e) {
+                                System.err.println("Interrupted while polling. Ignoring");
+                            }
+                        }
+
+                        if (!disconnecting) {
+                            String message = gameField.getWinner() + " Would you like to ask them to play again?";
+                            boolean myPlayAgain = //??? "Play Again Y/n"
+
+                            if (myPlayAgain == true) {
+                                writer.println("PLAY_AGAIN");
+                                String playAgain = reader.readLine();
+                                if (playAgain != null) {
+                                    switch (playAgain) {
+                                        case "YES":
+                                            startNewGame = true;
+                                            break;
+                                        case "DISCONNECT":
+                                            keepPlaying = false;
+                                            startNewGame = false;
+                                            disconnecting = true;
+                                            break;
+                                        default:
+                                            System.err.println("Warning. Unexpected response: " + playAgain + ". Not playing again.");
+                                    }
+                                }
+                            }
+                        }
                     }
+
+
+                }
+                if (socket.isConnected()) {
+                    // say goodbye
+                    writer.println("DISCONNECT");
+                    socket.close();
+                }
+            }
+        } catch(
+        SocketException se)
+
+        {
+            System.err.println("Disconnecting. Closing down server.");
+            keepListening = false;
+        } catch(
+        IOException ioe)
+
+        {
+            System.err.println("Networking error. Closing down server.");
+            ioe.printStackTrace();
+            keepListening = false;
+        } catch(
+        Exception e)
+
+        {
+            System.err.println("Other exception occurred. Closing down server.");
+            e.printStackTrace();
+            keepListening = false;
+        } finally
+
+        {
+            try {
+                if (socket != null && !socket.isClosed()) {
+                    socket.close();
+                }
+            } catch (IOException closingException) {
+                System.err.println("Error closing client socket: " + closingException.getMessage());
+            }
+        }
+
+        public void stopListening() {
+            if (listener != null) {
+                try{
+                    listener.close();
+                } catch (IOException ioe){
+                    System.err.println("Error disconnecting listener: " + ioe.getMessage());
                 }
             }
         }
     }
 
+//    class Client implements Runnable{
+//
+//    }
 
 
 //    @Override
